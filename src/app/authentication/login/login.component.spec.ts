2563150a5b56931942ace04564537ce8e791ab92
Tim Walshjamin <walshta@users.noreply.github.com>
2025-10-08 09:03:21 -0400

    chore(): increase code coverage

--- src/app/authentication/login/login.component.spec.ts
+++ src/app/authentication/login/login.component.spec.ts
@@ -1,8 +1,10 @@
-import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
+import { TokenService } from '../token/token.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
+  let tokenServiceSpy: jasmine.SpyObj<TokenService>;
   let fixture: ComponentFixture<LoginComponent>;
 
   (globalThis as any).google = {
@@ -16,9 +18,14 @@ describe('LoginComponent', () => {
   };
 
   beforeEach(waitForAsync(() => {
+    const spy = jasmine.createSpyObj('TokenService', ['setToken']);
+
     TestBed.configureTestingModule({
       imports: [LoginComponent],
+      providers: [{ provide: TokenService, useValue: spy }]
     });
+
+    tokenServiceSpy = TestBed.inject(TokenService) as jasmine.SpyObj<TokenService>;
   }));
 
   beforeEach(() => {
@@ -34,5 +41,13 @@ describe('LoginComponent', () => {
   it('should render Google Sign-In button container', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('#google-signin-button')).toBeTruthy();
+  });
+
+  it('should save the JWT token', () => {
+    const response = {
+      credential: 'test-jwt-token'
+    };
+    component.handleCredentialResponse(response);
+    expect(tokenServiceSpy.setToken).toHaveBeenCalledWith('test-jwt-token');
   });
 });
